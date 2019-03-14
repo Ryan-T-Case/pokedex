cd64019721038d2077c29c3ea2f99f3a7bb0bf0d
Ryan Case <Ryan_Case@outlook.com>
2019-03-13 22:45:24 -0400

    Installed passport js package and set up home page route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+// Import React Router Dependency
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Import Page Components to be handled by React Router
+import Home from "./pages/Home/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Components go here
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
