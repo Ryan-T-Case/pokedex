3de31e45ec53e0d911596769023cf1f48ea36487
Ryan Case <Ryan_Case@outlook.com>
2019-03-15 21:34:58 -0400

    Refractored the sign up page component to a stateful component, added methods to track input change and submit form data for signing up

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 
-const SignUpForm = () => {
+const SignUpForm = (props) => {
     return(
         <div className="SignUpForm">
-            <form>
+            <form onSubmit={props.handleSignUpFormSubmit}>
                 <label htmlFor="username">Let's begin with your name. What is it?</label>
-                <input type="text" name="username" placeholder="Create a unique username"></input>
+                <input type="text" name="username" placeholder="Create a unique username" value={props.usernameInput} onChange={props.handleSignUpFormInputChange}></input>
                 <br></br>
                 <label htmlFor="password">Password</label>
-                <input type="text" name="password" placeholder="Create a unique password"></input>
+                <input type="text" name="password" placeholder="Create a unique password" value={props.passwordInput} onChange={props.handleSignUpFormInputChange}></input>
                 <br></br>
                 <label htmlFor="gender">Are you a Boy or a Girl?</label>
-                <input type="radio" name="gender" value="Boy"></input><span>Boy</span>
-                <input type="radio" name="gender" value="Girl"></input><span>Girl</span>
+                <input type="radio" name="gender" value="Boy" onChange={props.handleSignUpFormInputChange}></input><span>Boy</span>
+                <input type="radio" name="gender" value="Girl" onChange={props.handleSignUpFormInputChange}></input><span>Girl</span>
                 <br></br>
                 <label htmlFor="starter">Now, which starter Pokemon do you want?</label>
-                <input type="radio" name="starter" value="Bulbasaur"></input><span>Bulbasaur</span>
-                <input type="radio" name="starter" value="Charmander"></input><span>Charmander</span>
-                <input type="radio" name="starter" value="Squirtle"></input><span>Squirtle</span>
+                <input type="radio" name="starter" value="Bulbasaur" onChange={props.handleSignUpFormInputChange}></input><span>Bulbasaur</span>
+                <input type="radio" name="starter" value="Charmander" onChange={props.handleSignUpFormInputChange}></input><span>Charmander</span>
+                <input type="radio" name="starter" value="Squirtle" onChange={props.handleSignUpFormInputChange}></input><span>Squirtle</span>
                 <br></br>
                 <input type="submit" value="Submit"></input>
             </form>
--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,14 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
 import "./SignUp.css"
 import SignUpForm from "../../components/SignUpForm";
 
-const SignUp = () => {
-    return(
-        <div className="SignUp">
-            <h1>This is the Sign Up Page.</h1>
-            <SignUpForm />
-        </div>
-    )
+class SignUp extends Component {
+    state = {
+        signUpFormInput: {
+            username: "",
+            password: "",
+            gender: "",
+            starter: ""
+        }
+    }
+
+    handleSignUpFormInputChange = (event) => {
+        //Getting the input value
+        let inputValue = event.target.value;
+
+        //Getting the input name
+        let inputName = event.target.name;
+
+        //Use the name of the input we captured to determine what input gets updated in state
+        if (inputName === "username") {
+            this.setState(prevState => ({
+                signUpFormInput: {
+                    ...prevState.signUpFormInput,
+                    username: inputValue
+                }
+            }));
+        } else if (inputName === "password") {
+            this.setState(prevState => ({
+                signUpFormInput: {
+                    ...prevState.signUpFormInput,
+                    password: inputValue
+                }
+            }));
+        } else if (inputName === "gender") {
+            this.setState(prevState => ({
+                signUpFormInput: {
+                    ...prevState.signUpFormInput,
+                    gender: inputValue
+                }
+            }));
+        } else if (inputName === "starter") {
+            this.setState(prevState => ({
+                signUpFormInput: {
+                    ...prevState.signUpFormInput,
+                    starter: inputValue
+                }
+            }));
+        }
+    }
+
+    handleSignUpFormSubmit = (event) => {
+        event.preventDefault();
+        //Print the sign in credentials of the user
+        console.log(`
+        ------------------------------
+        User Signed Up Successfully!
+        ------------------------------
+        Account Info:
+
+        Username: ${this.state.signUpFormInput.username}
+        Password: ${this.state.signUpFormInput.password}
+        Gender: ${this.state.signUpFormInput.gender}
+        Starter Pokemon: ${this.state.signUpFormInput.starter}
+        ------------------------------
+        `)
+        // Clear the form input
+        this.setState({
+            signUpFormInput: {
+                username: "",
+                password: "",
+                gender: "",
+                starter: ""
+            }
+        })
+    }
+
+    render () {
+        return(
+            <div className="SignUp">
+                <h1>This is the Sign Up Page.</h1>
+                <SignUpForm handleSignUpFormInputChange={this.handleSignUpFormInputChange} handleSignUpFormSubmit={this.handleSignUpFormSubmit} usernameInput={this.state.signUpFormInput.username} passwordInput={this.state.signUpFormInput.password} genderInput={this.state.signUpFormInput.gender} starterInput={this.state.signUpFormInput.starter}/>
+            </div>
+        )
+    }
 }
 
 export default SignUp;
